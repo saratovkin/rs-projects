0ce1051c529bd248dc485adf7761bc63f761ca6e
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-30 19:05:21 +0300

    feat: add all resolutions adaptive layout for header and welcome,visitng and explore sections

--- museum-adaptive/js/burger-menu.js
+++ museum-adaptive/js/burger-menu.js
@@ -1,14 +1,37 @@
-let indicator = true;
-
-function showSideNav(x) {
+function burgerClick(x) {
   x.classList.toggle("change");
-  if(indicator){
-    document.querySelector('.welcome-text').style.opacity = '0';
-    document.querySelector('.navigation-scaled').style.display = 'block';
+  if (indicator) {
+    showNav()
     indicator = false;
   } else {
-    document.querySelector('.welcome-text').style.opacity = '1';
-    document.querySelector('.navigation-scaled').style.display = 'none';
+    hideNav();
     indicator = true;
-  } 
+  }
 }
+
+function showNav() {
+  navScaled.style.display = 'block';
+  navScaled.classList.remove('.hide-nav');
+  navScaled.classList.add('.show-nav');
+  document.querySelector('.welcome-text').style.opacity = '0';
+}
+
+function hideNav() {
+  navScaled.classList.add('hide-nav');
+  navScaled.classList.remove('show-nav');
+  document.querySelector('.welcome-text').style.opacity = '1';
+  setTimeout(function () { navScaled.style.display = 'none'; }, 500);
+}
+
+// window.onload = function () {
+//   document.onclick = function (e) {
+//     if (e.target != navScaled) {
+//       hideForm();
+//     }
+//   };
+// };
+
+
+let indicator = true;
+let navScaled = document.querySelector('.navigation-scaled');
+let welcomeText = document.querySelector('.welcome-text');
