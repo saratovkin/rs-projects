ee05a121654fd6249fd87b56e85feb3bd5ed9e5b
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-10-03 19:52:56 +0300

    feat: add popup navigation menu

--- museum-adaptive/js/burger-menu.js
+++ museum-adaptive/js/burger-menu.js
@@ -1,37 +1,54 @@
-function burgerClick(x) {
-  x.classList.toggle("change");
-  if (indicator) {
-    showNav()
-    indicator = false;
-  } else {
-    hideNav();
-    indicator = true;
+function burgerClick() {
+  if (timer) {
+    burgerIcon.classList.toggle("change");
+    indicator ? showNav() : hideNav();
+    timer = false;
+    setTimeout(function () { timer = true; }, 500);
   }
 }
 
 function showNav() {
+  indicator = false;
   navScaled.style.display = 'block';
-  navScaled.classList.remove('.hide-nav');
-  navScaled.classList.add('.show-nav');
-  document.querySelector('.welcome-text').style.opacity = '0';
+  navScaled.classList.remove('hide-nav');
+  navScaled.classList.add('show-nav');
+  welcomeText.classList.remove('show-overlay');
+  welcomeText.classList.add('hide-overlay');
 }
 
 function hideNav() {
-  navScaled.classList.add('hide-nav');
+  indicator = true;
   navScaled.classList.remove('show-nav');
-  document.querySelector('.welcome-text').style.opacity = '1';
+  navScaled.classList.add('hide-nav');
+  welcomeText.classList.remove('hide-overlay');
+  welcomeText.classList.add('show-overlay');
   setTimeout(function () { navScaled.style.display = 'none'; }, 500);
 }
 
-// window.onload = function () {
-//   document.onclick = function (e) {
-//     if (e.target != navScaled) {
-//       hideForm();
-//     }
-//   };
-// };
+window.onload = function () {
+  document.onclick = function (e) {
+    let clickCheck = true;
+    switch (e.target) {
+      case burgerIcon:
+        clickCheck = false;
+      case burgerIcon.childNodes.item(1):
+        clickCheck = false;
+      case burgerIcon.childNodes.item(3):
+        clickCheck = false;
+      case burgerIcon.childNodes.item(5):
+        clickCheck = false;
+      case navScaled:
+        clickCheck = false;
+    }
+    if (clickCheck && !indicator) {
+      burgerClick();
+    }
+  };
+};
 
 
 let indicator = true;
+let timer = true;
 let navScaled = document.querySelector('.navigation-scaled');
 let welcomeText = document.querySelector('.welcome-text');
+let burgerIcon = document.querySelector('.header-burger-menu');
