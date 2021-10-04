f8b4f195bd02bd13bc9eaf20d820de80cac0ef08
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-10-04 16:43:03 +0300

    feat: add iframe video optimization

---
+++ museum-adaptive/js/youtube-video.js
@@ -0,0 +1,52 @@
+function findVideos() {
+  let videos = document.querySelectorAll('.video-slider-item');
+
+  for (let i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video) {
+  let link = video.querySelector('.video-link');
+  let media = video.querySelector('.video-preview');
+  let button = video.querySelector('.video-slider-play');
+  let id = parseMediaURL(media);
+
+  video.addEventListener('click', () => {
+    let iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    media.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('enabled');
+}
+
+function parseMediaURL(media) {
+  let regexp = /https:\/\/img\.youtube\.com\/vi\/([a-zA-Z0-9_-]+)\/0\.jpg/i;
+  let url = media.src;
+  let match = url.match(regexp);
+  return match[1];
+}
+
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video-preview');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
