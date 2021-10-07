62bbf600b70eead93e537551baf26d29841a5949
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-10-07 21:44:12 +0300

    init: start museum-dom task

---
+++ museum-dom/js/tickets-calculator.js
@@ -0,0 +1,119 @@
+function showSelect() {
+  select.style.display = 'block';
+  listIcon.style.transform = 'rotate(180deg)';
+}
+
+function setTicketType(type) {
+  let checkoutType = document.querySelector('.payment-type');
+  let selectType = document.querySelector('.chosen-type');
+  let radioType = document.querySelectorAll('.custom-radio');
+  if (type == 0) {
+    checkoutType.innerHTML = 'Permanent exhibition';
+    selectType.innerHTML = 'Permanent exhibition';
+    radioType[0].checked = true;
+    ticketPrice = 20;
+  }
+  if (type == 1) {
+    checkoutType.innerHTML = 'Temporary exhibition';
+    selectType.innerHTML = 'Temporary exhibition';
+    radioType[1].checked = true;
+    ticketPrice = 25;
+  }
+  if (type == 2) {
+    checkoutType.innerHTML = 'Combined Admission';
+    selectType.innerHTML = 'Combined Admission';
+    radioType[2].checked = true;
+    ticketPrice = 40;
+  }
+  refreshPrice();
+  document.querySelector('.select-drop-down').style.display = 'none';
+  document.querySelector('.select-list').style.transform = '';
+  localStorage.setItem('type', type);
+  localStorage.setItem('ticketPrice', ticketPrice);
+}
+
+//0 is senior, 1 is basic
+function addTicket(age) {
+  if (age == 1 && amountBasic < 20) {
+    amountBasic++;
+  }
+  if (age == 0 && amountSenior < 20) {
+    amountSenior++;
+  }
+  refreshCounter();
+  refreshPrice();
+  toggleCheckout(age);
+  localStorage.setItem('amountBasic', amountBasic);
+  localStorage.setItem('amountSenior', amountSenior);
+}
+
+function removeTicket(age) {
+  if (age == 1 && amountBasic > 0) {
+    amountBasic--;
+  }
+  if (age == 0 && amountSenior > 0) {
+    amountSenior--;
+  }
+  refreshCounter();
+  refreshPrice();
+  toggleCheckout(age);
+  localStorage.setItem('amountBasic', amountBasic);
+  localStorage.setItem('amountSenior', amountSenior);
+}
+
+function refreshCounter() {
+  document.querySelectorAll('.tickets-amount.basic')[0].value = amountBasic;
+  document.querySelectorAll('.tickets-amount.basic')[1].value = amountBasic;
+  document.querySelectorAll('.tickets-amount.senior')[0].value = amountSenior;
+  document.querySelectorAll('.tickets-amount.senior')[1].value = amountSenior;
+  document.getElementById('basic-counter').innerHTML = amountBasic;
+  document.getElementById('senior-counter').innerHTML = amountSenior;
+}
+
+function refreshPrice() {
+  document.querySelector('.sum').innerHTML = ticketPrice * amountBasic + ticketPrice / 2 * amountSenior;
+  document.querySelector('.final-sum').innerHTML = ticketPrice * amountBasic + ticketPrice / 2 * amountSenior + " €";
+  document.querySelector('.basic-sum').innerHTML = ticketPrice * amountBasic + " €";
+  document.querySelector('.senior-sum').innerHTML = ticketPrice / 2 * amountSenior + " €";
+  document.querySelectorAll('.basic-price')[0].innerHTML = `Basic 18+ (${ticketPrice} €)`;
+  document.querySelectorAll('.basic-price')[1].innerHTML = `Basic (${ticketPrice} €)`;
+  document.querySelectorAll('.senior-price')[0].innerHTML = `Senior 65+ (${ticketPrice / 2} €)`;
+  document.querySelectorAll('.senior-price')[1].innerHTML = `Senior (${ticketPrice / 2} €)`;
+}
+
+//0 is senior, 1 is basic
+function toggleCheckout(age) {
+  if (age == 1) {
+    if (amountBasic == 0) {
+      document.querySelector('.payment-subtotal.basic').style.display = 'none';
+    } else {
+      document.querySelector('.payment-subtotal.basic').style.display = 'flex';
+    }
+  }
+  if (age == 0) {
+    if (amountSenior == 0) {
+      document.querySelector('.payment-subtotal.senior').style.display = 'none';
+    } else {
+      document.querySelector('.payment-subtotal.senior').style.display = 'flex';
+    }
+  }
+}
+
+let select = document.querySelector('.select-drop-down');
+let listIcon = document.querySelector('.select-list');
+
+let ticketPrice = 20;
+let amountBasic = 1;
+let amountSenior = 1;
+let typeOfExh = 0;
+
+window.onload = function () {
+  ticketPrice = +localStorage.getItem('ticketPrice') || 20;
+  typeOfExh = +localStorage.getItem('type') || 0;
+  amountBasic = +localStorage.getItem('amountBasic') || 1;
+  amountSenior = +localStorage.getItem('amountSenior') || 1;
+  setTicketType(typeOfExh);
+  refreshCounter();
+  refreshPrice();
+};
+
---
+++ museum-dom/js/videoplayer.js
@@ -0,0 +1,156 @@
+function changeProgressBar() {
+    let progress = document.getElementById('progress-bar');
+    let displayProgress = document.getElementById('bar-sub');
+    displayProgress.style.width = `${progress.value}%`;
+}
+
+function changeVolumeBar() {
+    let volume = document.getElementById('volume-bar');
+    let volumeRange = document.getElementById('volume-sub');
+    volumeRange.style.width = `${volume.value}%`;
+}
+
+function videoPlay() {
+    if (videoPlayer.paused) {
+        videoPlayer.play();
+        playIcon.style.display = 'none';
+        stopIcon.style.display = 'block';
+        playIconMain.style.display = 'none';
+    } else {
+        videoPlayer.pause();
+        playIcon.style.display = 'block';
+        stopIcon.style.display = 'none';
+        playIconMain.style.display = 'block';
+    }
+}
+
+function showDefault() {
+    playIcon.style.display = 'block';
+    stopIcon.style.display = 'none';
+    playIconMain.style.display = 'block';
+}
+
+function videoProgress() {
+    progressBar.value = (videoPlayer.currentTime / videoPlayer.duration) * 100;
+    changeProgressBar();
+}
+
+function videoChangeTime() {
+    if (progressBar.value == 100) {
+        videoPlay();
+    }
+    videoPlayer.currentTime = (videoPlayer.duration * progressBar.value) / 100;
+}
+
+function videoChangeVolume() {
+    videoPlayer.volume = volumeBar.value / 100;
+    if (videoPlayer.volume == 0) {
+        volumeIcon.style.display = 'none';
+        muteIcon.style.display = 'block';
+    } else {
+        volumeIcon.style.display = 'block';
+        muteIcon.style.display = 'none';
+    }
+    changeVolumeBar();
+}
+
+function videoMute() {
+    if (videoPlayer.volume == 0) {
+        volumeBar.value = tempVolume;
+        videoPlayer.volume = volumeBar.value / 100;
+        volumeIcon.style.display = 'block';
+        muteIcon.style.display = 'none';
+    } else {
+        tempVolume = volumeBar.value;
+        videoPlayer.volume = 0;
+        volumeBar.value = 0;
+        volumeIcon.style.display = 'none';
+        muteIcon.style.display = 'block';
+    }
+    changeVolumeBar();
+}
+
+function fullScreenVideo() {
+    if (fullScreenFlag) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        fullScreenIcon.style.display = 'block';
+        smallScreenIcon.style.display = 'none';
+        fullScreenFlag = false;
+    } else {
+        if (videoDiv.requestFullscreen) {
+            videoDiv.requestFullscreen();
+        }
+        fullScreenIcon.style.display = 'none';
+        smallScreenIcon.style.display = 'block';
+        fullScreenFlag = true;
+    }
+}
+
+function videoReduceSpeed() {
+    videoPlayer.playbackRate = (videoPlayer.playbackRate - 0.25) || 0.25;
+    showSpeed();
+}
+
+function videoIncreaseSpeed() {
+    if (videoPlayer.playbackRate < 2) {
+        videoPlayer.playbackRate = (videoPlayer.playbackRate + 0.25);
+    }
+    showSpeed();
+}
+
+function showSpeed() {
+    clearTimeout(speedTimeout);
+    document.getElementById('speed').innerHTML = videoPlayer.playbackRate + 'x';
+    document.querySelector('.display-speed').classList.add('show-speed');
+    speedTimeout = setTimeout(function () {
+        document.querySelector('.display-speed').classList.remove('show-speed');
+    }, 1000);
+}
+
+function videoHotKeys(e) {
+    if (e.target.type === 'text' || e.target.type === 'tel' || e.target.type === 'email') {
+        return;
+    }
+    if ((e.shiftKey && e.key === '>') || (e.shiftKey && e.key === 'Ю')) {
+        videoIncreaseSpeed();
+    }
+    if ((e.shiftKey && e.key === '<') || (e.shiftKey && e.key === 'Б')) {
+        videoReduceSpeed();
+    }
+    if (e.key === 'm' || e.key === 'M' || e.key === 'ь' || e.key === 'Ь') {
+        videoMute();
+    }
+    if (e.key === 'f' || e.key === 'F' || e.key === 'а' || e.key === 'А') {
+        fullScreenVideo();
+    }
+    if (e.code == 'Space') {
+        console.log('why');
+        e.preventDefault();
+        videoPlay();
+    }
+}
+
+let videoPlayer = document.querySelector('.video-player');
+
+let progressBar = document.getElementById('progress-bar');
+let volumeBar = document.getElementById('volume-bar');
+
+let playIcon = document.getElementById('play');
+let playIconMain = document.getElementById('playMain');
+let stopIcon = document.getElementById('pause');
+let volumeIcon = document.getElementById('unmute');
+let muteIcon = document.getElementById('mute')
+let fullScreenIcon = document.getElementById('enter-fullscreen');
+let smallScreenIcon = document.getElementById('exit-fullscreen');
+
+let videoDiv = document.querySelector('.videoplayer');
+
+let tempVolume = 100;
+let fullScreenFlag = false;
+let speedTimeout;
+
+videoPlayer.addEventListener('timeupdate', videoProgress);
+videoPlayer.addEventListener('ended', showDefault);
+document.addEventListener('keydown', videoHotKeys, false);
