b9afa6808cf026e57d8270cbcfdfa8234278aea4
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-12-06 23:14:00 +0300

    fix: change api url for netlify  support

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     public constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'c422559746fd4bea8cd0c5c4acecb484',
         });
     }
