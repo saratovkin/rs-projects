9a766a4d1946f89b2113006eed02d7e16fce6e2f
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-12-12 17:49:49 +0300

    fix: implement correct work of filtration by shape, color and size

--- christmas-task/src/components/app/app.ts
+++ christmas-task/src/components/app/app.ts
@@ -13,21 +13,32 @@ function toggle(arr: any[], item: any) {
     return arr;
 }
 
-// interface ICondition {
-//     shape: string[];
-//     color: string[];
-//     size: string[];
-//     isFav: boolean;
-// }
+interface ICondition {
+    shape: string[];
+    color: string[];
+    size: string[];
+    favorite: string;
+}
 
 class App {
     private controller: AppController;
     private view: AppView;
     public data: any[];
 
+    private condition: ICondition;
+
     public constructor() {
         this.controller = new AppController();
         this.view = new AppView();
+        this.condition = {
+            // shape: ['шар', 'фигурка', 'колокольчик', 'шишка', 'снежинка'],
+            // color: ['желтый', 'зелёный', 'красный', 'белый', 'синий'],
+            // size: ['малый', 'средний', 'большой'],
+            shape: [],
+            color: [],
+            size: [],
+            favorite: 'false',
+        }
     }
 
     public async start() {
@@ -37,23 +48,57 @@ class App {
         this.initFilter();
     }
 
+    private updateCondition(type: string, param: string) {
+        if (type === 'shape') {
+            toggle(this.condition.shape, param);
+        }
+        if (type === 'color') {
+            toggle(this.condition.color, param);
+        }
+        if (type === 'size') {
+            toggle(this.condition.size, param);
+        }
+        if (type === 'favorite') {
+            this.condition.favorite = param;
+        }
+    }
+
+    private filterFunc(e: any) {
+        let res = true;
+        if (this.condition.shape.length != 0) {
+            res = this.condition.shape.includes(e.shape);
+            if (!res) {
+                return false;
+            }
+        }
+        if (this.condition.color.length != 0) {
+            res = this.condition.color.includes(e.color);
+            if (!res) {
+                return false;
+            }
+        }
+        if (this.condition.size.length != 0) {
+            res = this.condition.size.includes(e.size);
+            if (!res) {
+                return false;
+            }
+        }
+        return res;
+    }
+
     public filter(type: string, e: any) {
         const param = e.target.getAttribute('filter');
         let filtered;
         if (param) {
-            e.target.classList.toggle('clicked');
-            if (param === 'false' || param === 'true') {
-                if (param === 'true') {
-                    e.target.setAttribute('filter', 'false');
-                    filtered = this.data;
-                } else {
-                    e.target.setAttribute('filter', 'true');
-                    filtered = this.data.filter(word => word[type] == Boolean(param));
-                }
-            } else {
-                filtered = this.data.filter(word => word[type] == param);
-            }
             this.view.clear();
+            this.updateCondition(type, param);
+            e.target.classList.toggle('clicked');
+            // filtered = this.data.filter((e) => {
+            //     return this.condition.shape.includes(e.shape) &&
+            //         this.condition.color.includes(e.color) &&
+            //         this.condition.size.includes(e.size);
+            // });
+            filtered = this.data.filter(e => this.filterFunc(e));
             this.view.draw(filtered);
         }
     }
@@ -71,3 +116,17 @@ class App {
 }
 
 export default App;
+
+        // if (param === 'false' || param === 'true') {
+            //     if (param === 'true') {
+            //         e.target.setAttribute('filter', 'false');
+            //         filtered = this.data;
+            //     } else {
+            //         e.target.setAttribute('filter', 'true');
+            //         filtered = this.data.filter(word => word[type] == Boolean(param));
+            //     }
+            // } else {
+            //     filtered = this.data.filter(word => word[type] == param);
+            // }
+            // this.view.clear();
+            // this.view.draw(filtered);
