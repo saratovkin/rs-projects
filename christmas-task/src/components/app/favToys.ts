621b2a0d15510a65d9f0fde5098313f334ca0232
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-12-17 13:09:22 +0300

    fix: change style of toys limit modal window

--- christmas-task/src/components/app/favToys.ts
+++ christmas-task/src/components/app/favToys.ts
@@ -5,6 +5,7 @@ const favLimit: number = 20;
 class FavToys {
 
   public favList: string[];
+  private timer: NodeJS.Timer;
   private savedSettings: SavedSettings;
   constructor() {
     this.savedSettings = new SavedSettings;
@@ -14,22 +15,28 @@ class FavToys {
 
   public toggleFav(e: Event) {
 
-    let index: string = (e.target as HTMLImageElement).alt; 
+    let index: string = (e.target as HTMLImageElement).alt;
     if (this.favList.includes(index)) {
       this.favList.splice(this.favList.indexOf(index), 1);
-      ((e.target as HTMLElement).parentNode as HTMLElement).classList.remove('fav-toy'); 
+      ((e.target as HTMLElement).parentNode as HTMLElement).classList.remove('fav-toy');
     } else if (this.favList.length < favLimit) {
       this.favList.push(index);
-      ((e.target as HTMLElement).parentNode as HTMLElement).classList.add('fav-toy'); 
+      ((e.target as HTMLElement).parentNode as HTMLElement).classList.add('fav-toy');
     } else {
-      alert('limit!');
+      this.showAlertMessage(e);
     }
     this.savedSettings.setFavToys(this.favList);
     this.updateCounter();
   }
 
+  private showAlertMessage(e: Event) {
+    (e.target as HTMLElement).parentElement.querySelector('.fav-limit').classList.remove('hide');
+    this.timer = setTimeout(() =>
+      (e.target as HTMLElement).parentElement.querySelector('.fav-limit').classList.add('hide'), 1500);
+  }
+
   private updateCounter() {
-    document.querySelector('.fav-counter').textContent = this.favList.length.toString();
+    document.querySelector('.fav-count').textContent = this.favList.length.toString();
   }
 }
 
