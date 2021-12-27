914252ff9608b688352b72ab2cd12dbd06f4ce15
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-12-27 17:07:39 +0300

    refactor: split class into two separate

--- christmas-task/src/components/view/toysView.ts
+++ christmas-task/src/components/view/toysView.ts
@@ -2,6 +2,8 @@ import './toys-picker.css';
 
 import IToy from '../interfaces/IToy';
 
+const droppableArea: HTMLElement = document.querySelector('.drop-zone') as HTMLElement;
+
 class ToysView {
   private static createToy(toy: IToy): void {
     const toyElem = document.createElement('div');
@@ -15,70 +17,11 @@ class ToysView {
     toyCounter.textContent = toy.count;
     toyElem.appendChild(toyCounter);
     toyElem.appendChild(toyImg);
-    ToysView.moveToy(toyImg);
+    const toyMove = new ToyMove(toyImg);
+    toyMove.generateClones();
     document.querySelector('.toys-picker')?.appendChild(toyElem);
   }
 
-  // TODO fix count bug
-  // if enough time refactor as separate class
-  public static moveToy(img: HTMLImageElement) {
-    const toyImg: HTMLImageElement = img;
-    const toyCounter: HTMLDivElement = img.parentNode?.querySelector('.toy-counter') as HTMLDivElement;
-    const droppableArea: HTMLElement = document.querySelector('.test-path') as HTMLElement;
-    let droppableFlag = false;
-    let toyCount: number;
-    let toyClone: HTMLImageElement;
-    toyImg.ondragstart = function () {
-      return false;
-    };
-    toyImg.onmousedown = function (event) {
-      function moveAt(pageX: number, pageY: number) {
-        toyImg.style.left = `${pageX - toyImg.offsetWidth / 2}px`;
-        toyImg.style.top = `${pageY - toyImg.offsetHeight / 2}px`;
-      }
-      function onMouseMove(e: MouseEvent) {
-        moveAt(e.pageX, e.pageY);
-      }
-      toyCount = +(toyImg.getAttribute('data-count') as string);
-      if (toyCount >= +(toyCounter.textContent as string)) {
-        toyCounter.textContent = (toyCount - 1).toString();
-      }
-      if (toyCount > 1) {
-        toyCount = +(toyCounter.textContent as string);
-        toyImg.setAttribute('data-count', '1');
-        toyClone = toyImg.cloneNode(true) as HTMLImageElement;
-        toyClone.setAttribute('data-count', toyCount.toString());
-        toyImg.parentNode?.appendChild(toyClone);
-        ToysView.moveToy(toyClone);
-      }
-      toyImg.style.position = 'absolute';
-      document.body.append(toyImg);
-      moveAt(event.pageX, event.pageY);
-      toyImg.style.position = 'absolute';
-      document.body.append(toyImg);
-      moveAt(event.pageX, event.pageY);
-      document.addEventListener('mousemove', onMouseMove);
-      toyImg.onmouseup = function () {
-        if (droppableFlag) {
-          document.removeEventListener('mousemove', onMouseMove);
-          toyImg.onmouseup = null;
-        } else {
-          toyCounter.textContent = (+(toyCounter.textContent as string) + 1).toString();
-          toyImg.remove();
-        }
-      };
-    };
-    toyImg.onmousemove = function (event) {
-      toyImg.hidden = true;
-      const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-      toyImg.hidden = false;
-      if (elemBelow === droppableArea) {
-        droppableFlag = true;
-      } else {
-        droppableFlag = false;
-      }
-    };
-  }
 
   public static clearToys(): void {
     document.querySelectorAll('.toy-option').forEach((item) => {
@@ -93,4 +36,147 @@ class ToysView {
   }
 }
 
+class ToyMove {
+  private toyImg: HTMLImageElement;
+
+  private toyContainer: HTMLDivElement;
+
+  private toyCounter: HTMLDivElement;
+
+  constructor(img: HTMLImageElement) {
+    this.toyImg = img;
+    this.toyContainer = img.parentNode as HTMLDivElement;
+    this.toyCounter = this.toyContainer.querySelector('.toy-counter') as HTMLDivElement;
+  }
+
+  public generateClones() {
+    let toyCount = +(this.toyImg.getAttribute('data-count') as string);
+    while (toyCount != 0) {
+      this.moveToy();
+      toyCount -= 1;
+    }
+  }
+
+  public moveToy() {
+    let droppableFlag = false;
+    let counter: number;
+    const toyImg: HTMLImageElement = this.toyImg;
+    const maxCount: number = +(toyImg.getAttribute('data-count') as string);
+    const toyClone: HTMLImageElement = this.toyImg.cloneNode(true) as HTMLImageElement;
+    const toyCounter: HTMLDivElement = this.toyCounter;
+    const toyContainer: HTMLDivElement = this.toyContainer;
+    toyClone.setAttribute('data-count', '1');
+    this.toyImg.parentNode?.appendChild(toyClone);
+    toyClone.ondragstart = function () {
+      return false;
+    };
+    toyClone.addEventListener('mousemove', (event) => {
+      toyClone.hidden = true;
+      const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      toyClone.hidden = false;
+      if (elemBelow === droppableArea) {
+        droppableFlag = true;
+      } else {
+        droppableFlag = false;
+      }
+    });
+    toyClone.addEventListener('mousedown', (event) => {
+      function moveAt(pageX: number, pageY: number) {
+        toyClone.style.left = `${pageX - toyClone.offsetWidth / 2}px`;
+        toyClone.style.top = `${pageY - toyClone.offsetHeight / 2}px`;
+      }
+      function onMouseMove(e: MouseEvent) {
+        moveAt(e.pageX, e.pageY);
+      }
+      toyClone.style.position = 'absolute';
+      document.querySelector('.toys-container')?.append(toyClone);
+      moveAt(event.pageX, event.pageY);
+      document.addEventListener('mousemove', onMouseMove);
+      toyClone.onmouseup = function () {
+        counter = (+(toyCounter.textContent as string))
+        if (droppableFlag) {
+          if (counter === 0) {
+            toyCounter.textContent = '0';
+          } else {
+            toyCounter.textContent = (counter - 1).toString();
+          }
+          if ((toyCounter.textContent as string) === '0') {
+            toyImg.classList.add('hide');
+          }
+          document.removeEventListener('mousemove', onMouseMove);
+          toyClone.onmouseup = null;
+        } else {
+          toyImg.classList.remove('hide');
+          if (counter < maxCount) {
+            toyCounter.textContent = (+(toyCounter.textContent as string) + 1).toString();
+          }
+          document.removeEventListener('mousemove', onMouseMove);
+          toyClone.onmouseup = null;
+          toyClone.style.top = '6px';
+          toyClone.style.left = '6px';
+          toyContainer.append(toyClone);
+        }
+      };
+    });
+  }
+}
+
 export default ToysView;
+
+// public static moveToy(img: HTMLImageElement) {
+//   const toyImg: HTMLImageElement = img;
+//   const toyContainer: HTMLDivElement = img.parentNode as HTMLDivElement;
+//   const toyCounter: HTMLDivElement = toyContainer.querySelector('.toy-counter') as HTMLDivElement;
+//   let droppableFlag = false;
+//   let toyCount = +(toyImg.getAttribute('data-count') as string);
+//   while (toyCount != 0) {
+//     let toyClone: HTMLImageElement = toyImg.cloneNode(true) as HTMLImageElement;
+//     toyClone.setAttribute('data-count', '1');
+//     toyImg.parentNode?.appendChild(toyClone);
+//     toyClone.ondragstart = function () {
+//       return false;
+//     };
+//     toyClone.onmousemove = function (event) {
+//       toyClone.hidden = true;
+//       const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+//       toyClone.hidden = false;
+//       if (elemBelow === droppableArea) {
+//         droppableFlag = true;
+//       } else {
+//         droppableFlag = false;
+//       }
+//     };
+//     toyClone.onmousedown = function (event) {
+//       function moveAt(pageX: number, pageY: number) {
+//         toyClone.style.left = `${pageX - toyClone.offsetWidth / 2}px`;
+//         toyClone.style.top = `${pageY - toyClone.offsetHeight / 2}px`;
+//       }
+//       function onMouseMove(e: MouseEvent) {
+//         moveAt(e.pageX, e.pageY);
+//       }
+//       toyClone.style.position = 'absolute';
+//       document.body.append(toyClone);
+//       moveAt(event.pageX, event.pageY);
+//       document.addEventListener('mousemove', onMouseMove);
+//       toyClone.onmouseup = function () {
+//         if (droppableFlag) {
+//           toyCounter.textContent = (+(toyCounter.textContent as string) - 1).toString();
+//           if ((toyCounter.textContent as string) === '0') {
+//             toyImg.classList.add('hide');
+//           }
+//           document.removeEventListener('mousemove', onMouseMove);
+//           toyClone.onmouseup = null;
+//         } else {
+//           toyImg.classList.remove('hide');
+//           toyCounter.textContent = (+(toyCounter.textContent as string) + 1).toString();
+//           document.removeEventListener('mousemove', onMouseMove);
+//           toyClone.onmouseup = null;
+//           toyClone.style.top = '6px';
+//           toyClone.style.left = '6px';
+//           toyContainer.append(toyClone);
+//         }
+//       };
+//     };
+//     toyCount -= 1;
+//   }
+// }
