42a8f08130dfac249b55f3e8b0d660ea50fc20a9
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2022-01-14 21:55:47 +0300

    init: start async-race task

---
+++ async-race/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const header = (
+  <div>
+    <h1>Async Race</h1>
+  </div>
+);
+ReactDom.render(header, document.getElementById('root'));
